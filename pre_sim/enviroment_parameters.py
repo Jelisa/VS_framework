cdacf43902d9717ca53c6cbc61e8c5eef00c5bda
jelisa <jelisa.iglesias@bsc.es>
2017-01-18 12:37:01 +0100

    new file:   pre_sim/enviroment_parameters.py 	modified:   pre_sim/sims_preparation.py I've modified the sims_preparation.py so it reads the defaults values for the parameters from an external file called enviroment_parameters.py, so the users can modify the dafult values to their own paths with more ease and keep track of the changes to the script without loosing these values.

---
+++ pre_sim/enviroment_parameters.py
@@ -0,0 +1,10 @@
+pele_folders = "/home/kqtw353/stay/external_software/PELErev12349"
+schrodinger_path = "/opt/az/schrodinger/schrodinger_suite/2016.03"
+plop_path = "/home/kqtw353/stay/work/PlopRotTemp/PlopRotTemp.py"
+conformational_template = "/home/kqtw353/stay/work/conformational_search_pele_template.conf"
+pele_license_path = "/home/kqtw353/stay/external_software/PELErev12279"
+default_every = 1
+constraints_strength = 0.2
+atoms2apply_constraints_default = [" CA "]
+mutations_program_path = "/home/kqtw353/stay/work/mutations_program/mutations_program.py"
+obc_param_generator_path = "/home/kqtw353/stay/work/OBCparamsgen/solventOBCParamsGenerator.py"
