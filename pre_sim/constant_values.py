5bf6306503f37aeeaf5580a226c0cdbdce88fb22
jelisa <jelisa.iglesias@bsc.es>
2017-02-06 14:32:22 +0100

    Changes to be committed: 	new file:   constant_values.py Added the missing file constant_values.py containing  the molecular weight table and the allowed formats.

---
+++ pre_sim/constant_values.py
@@ -0,0 +1,20 @@
+atomic_table_weights = {
+    "h": 1,
+    "c": 12,
+    "n": 14,
+    "o": 16,
+    "f": 19,
+    "na  ": 23,
+    "mg": 12,
+    "p": 31,
+    "s": 32,
+    "cl  ": 35,
+    "k": 39,
+    "ca  ": 20,
+    "zn": 65,
+    "br": 80,
+    "i": 127,
+    "b": 11
+}
+
+accepted_formats = "\.mae|\.pdb"  # This specifies the patterns for the accepted formats.
