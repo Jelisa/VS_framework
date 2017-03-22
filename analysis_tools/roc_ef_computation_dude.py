e8ccbc677085e1a2b56d61672891afa4d34db2d6
jelisa <jelisa.iglesias@gmail.com>
2017-03-22 14:30:14 +0100

    Changes to be committed: 	modified:   analysis_tools/roc_ef_computation_dude.py Solved a bug that made the program to not write the SF name at the begining.

--- analysis_tools/roc_ef_computation_dude.py
+++ analysis_tools/roc_ef_computation_dude.py
@@ -166,6 +166,7 @@ def write_ef_files(general_dictionary, filename_prefix):
     # for score, values in general_dictionary.iteritems():
     filename = "{0}_ef_values.csv".format(filename_prefix)
     fieldnames = general_dictionary['Exp_energy'].keys()
+    fieldnames.insert(0, 'Exp_energy')
     fieldnames.sort()
     ordered_sfs = ['Exp_energy'] + sorted([ x for x in general_dictionary.keys() if x != "Exp_energy"])
     with open(filename, 'w') as outfile:
